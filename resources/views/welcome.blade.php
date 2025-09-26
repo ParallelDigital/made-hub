297ac05c781a1b6ba9e5c8669bc30f975231a5e3
Parallel <info@parallel-digital.com>
2025-09-26 16:24:42 +0400

    style: adjust dumbbell icon dimensions and container spacing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1054,9 +1054,9 @@
                     <!-- Features -->
                     <div class="space-y-6 sm:space-y-7 max-w-xl mx-auto lg:mx-0">
                         <div class="flex items-start gap-4">
-                            <div class="flex-shrink-0 w-9 h-9 sm:w-10 sm:h-10 rounded-full bg-primary text-purple-300 flex items-center justify-center">
+                            <div class="flex-shrink-0 sm:w-10 sm:h-10 rounded-full bg-primary text-purple-300 flex items-center justify-center">
                                 <!-- dumbbell icon -->
-                                <svg class="svg-inline--fa fa-dumbbell" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="dumbbell" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z"></path></svg>
+                                <svg class="svg-inline--fa fa-dumbbell w-9 h-4" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="dumbbell" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z"></path></svg>
                             </div>
                             <div>
                                 <div class="font-semibold leading-tight">5 Classes Per Week</div>
