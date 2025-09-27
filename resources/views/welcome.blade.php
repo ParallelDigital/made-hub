22e486abe14d06c06dc9e5ba104643616584c704
Parallel <info@parallel-digital.com>
2025-09-27 09:55:24 +0400

    style: reduce opacity of background image in class passes section to 0.15

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -922,7 +922,7 @@
 
         <!-- Flexible Class Passes Section -->
         <div id="class-packages" class="relative bg-[#1a1a2e] text-white py-14 sm:py-16 lg:py-20 overflow-hidden">
-            <div aria-hidden="true" class="absolute inset-0 bg-cover bg-center opacity-50" style="background-image: url('{{ asset('class-2.jpg') }}');"></div>
+            <div aria-hidden="true" class="absolute inset-0 bg-cover bg-center" style="background-image: url('{{ asset('class-2.jpg') }}'); opacity: 0.15;"></div>
             <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="text-center mb-12">
                     <h3 class="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4">CLASS PASSESS</h3>
