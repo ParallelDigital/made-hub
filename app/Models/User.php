7a7e3961bf7ef22e49fc096d164eb569a56905bd
Parallel <info@parallel-digital.com>
2025-09-27 18:49:44 +0400

    feat: import UserPass model in User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\UserPass;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
