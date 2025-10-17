4154c76eab4e438026899d289b28735f4ea70794
Parallel <info@parallel-digital.com>
2025-10-17 11:56:08 +0100

    feat: add membership_id foreign key to users table

---
+++ database/migrations/2025_10_17_105213_add_membership_id_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('membership_id')->nullable()->after('pin_code');
+            $table->foreign('membership_id')->references('id')->on('memberships')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['membership_id']);
+            $table->dropColumn('membership_id');
+        });
+    }
+};
